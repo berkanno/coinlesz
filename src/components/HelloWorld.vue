e86b885208f1a7b7470a67a41198cf9853e17c64
berkano <berkanoral25@gmail.com>
2023-04-06 14:44:59 +0300

    Created

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar style="position: fixed" flat height="100%">
+        
+      <Toolbar /> 
+     
+    </v-app-bar>
+
+    <v-container>
+      <v-row>
+        <v-col cols="12"> </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue3";
+import Toolbar from '@/components/Toolbar.vue';
+export default {
+  components: {
+    AgGridVue,
+    Toolbar,
+  },
+  data() {
+    return {
+      columnDefs: [
+        { headerName: "area", field: "area" },
+        { headerName: "Common", field: "name.common" },
+        { headerName: "Region", field: "region" },
+      ] as any,
+      rowData: [] as any,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.coincap.io/v2/assets/bitcoin/markets")
+      .then((res: any) => console.log(res.data))
+      .catch((e: any) => console.log("err"));
+  },
+};
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-col cols="12" class="text-center bg-black text-h4">
+  <span class="text-uppercase text-overline text-red font-weight-black">
+   coin</span><span class="text-uppercase font-weight-thin" style="letter-spacing:10px;">lesz<v-icon icon="mdi-bitcoin"></v-icon></span>
+</v-col>
+
+
+</template>
